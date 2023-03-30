7a961ffbfe1293fc48491087a1baf2eea5f0c365
Kappappita <ignacioladal@gmail.com>
2023-03-29 23:42:43 -0300

    feat: Added user register with already exists comprobation

--- programa.rkt
+++ programa.rkt
@@ -1,10 +1,7 @@
 #lang racket
 
-(define (make-user name)
-    list name)
-
-(define (make-system name users drives drive-actual ruta-actual)
-    (list name users drives drive-actual ruta-actual))
+(define (make-system name users drives actual-user actual-drive actual-route )
+    (list name users drives actual-user actual-drive actual-route))
 
 (define (get-system-name sys)
     (car sys))
@@ -15,19 +12,67 @@
 (define (get-system-drives sys)
     caddr sys)
 
+
+(define (get-system-actual-user sys)
+    (cadddr sys))
+
+(define (get-system-actual-drive sys)
+    (cadddr (cdr sys)))
+
+(define (get-system-actual-route sys)
+    (cadddr (cddr sys)))
+
+(define (set-system-users sys users)
+    (make-system
+        get-system-name
+        users
+        get-system-drives
+        get-system-actual-user
+        get-system-actual-drive
+        get-system-actual-route))
+
 ;; Terminar de crear los getters
 
-
 (define (system name)
-    (make-system name null null null null))
+    (make-system name null null null null null))
 
-(define (get-users sys) (cadr sys))
+(define (make-user name)
+    (list name))
 
-(define (add-user username . users)
-    list users username)
+
+(define (add-user-to-users user-list user)
+    (reverse (cons user (reverse user-list))))
 ;;(define (register system name)
 
-(define S0 (system "newSystem"))
-(define S1 (get-system-users S0))
+(define (register sys user)
+    (if (member user (get-system-users sys)) sys 
+    (set-system-users sys (add-user-to-users (get-system-users sys) user))))
 
-S1
+(define S0 (system "newSystem"))
+(get-system-actual-route S0)
+
+
+
+
+
+
+
+
+
+
+(define S1
+    (register S0 "juan"))
+
+(define S2
+    (register S1 "dross"))
+
+(define S3
+    (register S2 "COÑOOOOOOOOO"))
+
+(define S4
+    (register S3 "dross"))
+
+(get-system-users S4)
+
+
+
